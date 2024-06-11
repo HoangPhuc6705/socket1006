b7439b9331d0a9df1dee8045f6a3547cc8f3d6d8
HoangPhuc6705 <tonguyenhoangphuc6705@gmail.com>
2024-06-11 08:07:29 +0700

    socket

--- socket.js
+++ socket.js
@@ -1,12 +1,16 @@
-const WebSocket = require('ws')
+const { WebSocket } = require('ws')
+const express = require('express')
+const { createServer } = require('http')
 
-const wss = new WebSocket.Server({ port: 5000 });
-console.log('Websocket server is running on PORT 5000');
+const app = express()
+const server = createServer(app)
+
+const wss = new WebSocket.Server({ server });
 
 const client = []
 wss.on('connection', (ws) => {
   console.log('A new client connected')
-
+  
   client.push(ws)
 
   ws.on('message', message => {
@@ -18,4 +22,13 @@ wss.on('connection', (ws) => {
       }
     })
   })
+})
+
+app.get('/', (req, res) => {
+  res.send('Websocket')
+})
+
+const PORT = process.env.PORT || 5000
+server.listen(PORT, () => {
+  console.log('Server is listening on PORT', PORT);
 })
